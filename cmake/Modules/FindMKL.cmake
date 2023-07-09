8074e21013098bba9b40f5f51db4789d31e9b3ba
Michael Sargado <michael.sargado@gmail.com>
2023-07-09 05:20:22 -0700

    cleaned up cmake-related files

--- cmake/Modules/FindMKL.cmake
+++ cmake/Modules/FindMKL.cmake
@@ -17,8 +17,6 @@
 #    target_link_libraries(TARGET ${MKL_LIBRARIES})
 #  endif()
 
-option (HAVE_MKL "Using BLAS/LAPACK and PARDISO from Intel MKL" OFF)
-
 # If already in cache, be silent
 if (MKL_INCLUDE_DIRS AND MKL_LIBRARIES)
     set (MKL_FIND_QUIETLY TRUE)
@@ -64,9 +62,6 @@ if (MKL_INCLUDE_DIR AND
     MKL_INTERFACE_LIBRARY AND
     MKL_THREAD_LIBRARY AND
     MKL_CORE_LIBRARY)
-
-#    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DUSE_MKL_BLAS -DUSE_MKL_PARDISO")
-#    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DUSE_MKL_BLAS -DUSE_MKL_PARDISO")
 else()
     set (MKL_INCLUDE_DIRS "")
     set (MKL_LIBRARIES "")
@@ -75,8 +70,7 @@ else()
     set (MKL_CORE_LIBRARY "")
 endif()
 
-# Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
-# all listed variables are TRUE.
+# Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if all listed variables are TRUE.
 include (FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(MKL DEFAULT_MSG MKL_LIBRARIES MKL_INCLUDE_DIRS MKL_INTERFACE_LIBRARY MKL_THREAD_LIBRARY MKL_CORE_LIBRARY)
 
