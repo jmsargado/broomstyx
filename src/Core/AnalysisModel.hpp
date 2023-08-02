a825c48f92ecc59aa13ad168cb7c86b4e9671c8b
Michael Sargado <michael.sargado@gmail.com>
2023-08-02 16:09:09 +0200

    Re-added all base classes.

---
+++ src/Core/AnalysisModel.hpp
@@ -0,0 +1,81 @@
+/*
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+#ifndef ANALYSISMODEL_HPP
+#define ANALYSISMODEL_HPP
+
+#include <cstdio>
+#include <string>
+#include "DofManager.hpp"
+#include "DomainManager.hpp"
+#include "MaterialManager.hpp"
+#include "NumericsManager.hpp"
+#include "OutputManager.hpp"
+#include "SolutionManager.hpp"
+#include "MeshReaders/MeshReader.hpp"
+
+namespace broomstyx
+{
+    class AnalysisModel final
+    {
+        friend AnalysisModel& analysisModel();
+        
+    public:
+        // Disable copy constructor and assignment operator
+        AnalysisModel( const AnalysisModel& ) = delete;
+        AnalysisModel& operator=( const AnalysisModel& ) = delete;
+        
+        void initializeYourself( std::string filename );
+        void solveYourself();
+        
+        DofManager&      dofManager();
+        DomainManager&   domainManager();
+        MaterialManager& materialManager();
+        NumericsManager& numericsManager();
+        OutputManager&   outputManager();
+        SolutionManager& solutionManager();
+        
+        MeshReader& meshReader();
+
+    private:
+        std::string _inputFilename;
+        std::string _meshFilename;
+        
+        DofManager*      _dofManager;
+        DomainManager*   _domainManager;
+        MaterialManager* _materialManager;
+        NumericsManager* _numericsManager;
+        OutputManager*   _outputManager;
+        SolutionManager* _solutionManager;
+
+        MeshReader*      _meshReader;
+
+        AnalysisModel();
+        virtual ~AnalysisModel();
+        
+        void readInputFile();
+        void readMeshReaderFrom( FILE* fp );
+    };
+    
+    AnalysisModel& analysisModel();
+}
+
+#endif /* ANALYSISMODEL_HPP */
---
+++ src/Core/MaterialManager.cpp
@@ -0,0 +1,111 @@
+/*
+  Copyright (c) 2014 - 2019 University of Bergen
+  
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+#include "MaterialManager.hpp"
+#include <omp.h>
+#include <stdexcept>
+#include <chrono>
+#include "AnalysisModel.hpp"
+#include "ObjectFactory.hpp"
+#include "Materials/Material.hpp"
+#include "Util/readOperations.hpp"
+
+using namespace broomstyx;
+
+MaterialManager::MaterialManager()
+{
+    _name = "MaterialManager";
+}
+
+MaterialManager::~MaterialManager()
+{
+#ifdef VERBOSE_DESTRUCTION
+    std::printf( "\n  Destroying MaterialManager... " );
+    std::fflush( stdout );
+#endif
+    
+    for ( int i = 0; i < (int)_material.size(); i++ )
+        if ( _material[ i ] )
+            delete _material[ i ];
+    
+#ifdef VERBOSE_DESTRUCTION
+    std::printf( "done." );
+    std::fflush( stdout );
+#endif
+}
+
+// Public methods
+// ----------------------------------------------------------------------------
+Material* MaterialManager::giveMaterial( int label )
+{
+    Material* target;
+    std::map<int,Material*>::iterator it = _material.find( label );
+    if ( it != _material.end() )
+        target = (*it).second;
+    else
+    {
+        throw std::runtime_error( "Error: There is no material corresponding to"
+                + std::string( " label '" ) + std::to_string( label )
+                + std::string( "'!\nSource: " ) + _name );
+    }
+    
+    return target;
+}
+// ----------------------------------------------------------------------------
+void MaterialManager::initializeMaterials()
+{
+    std::chrono::time_point<std::chrono::system_clock> tic, toc;
+    std::chrono::duration<double> tictoc;
+    
+    std::printf( "  %-40s", "Initializing materials ..." );
+    tic = std::chrono::high_resolution_clock::now();
+    
+    std::map<int,Material*>::iterator it;
+    for ( it = _material.begin(); it != _material.end(); it++ )
+        (*it).second->initialize();
+    
+    toc = std::chrono::high_resolution_clock::now();
+    tictoc = toc - tic;
+    std::printf( "done (time = %f sec.)\n", tictoc.count() );
+}
+// ----------------------------------------------------------------------------
+void MaterialManager::readMaterialsFrom( FILE* fp )
+{
+    int nMat = getIntegerInputFrom( fp, "Failed to read number of materials from input file!", _name );
+    
+    for ( int i = 0; i < nMat; i++ )
+    {
+        int label = getIntegerInputFrom( fp, "Failed reading material label from input file!", _name );
+        std::string materialName = getStringInputFrom( fp, "Failed reading material type from input file!", _name );
+        
+        Material* newMat = objectFactory().instantiateMaterial( materialName );
+        
+        std::pair< std::map<int,Material*>::iterator, bool> entry;
+        entry = _material.insert( std::pair<int,Material*>( label, newMat ) );
+        if ( !entry.second )
+            throw std::runtime_error( "Multiple declaration of materials for label '" + std::to_string( label )
+                + std::string( "' detected in input file!\nSource: " ) + _name );
+                
+        _material[ label ]->readParamatersFrom( fp );
+    }
+}
