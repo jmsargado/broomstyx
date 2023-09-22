f9f140ed3d020d9083d0bc0e26d87a5cc5b5c2fb
Michael Sargado <michael.sargado@gmail.com>
2023-09-22 03:12:13 +0200

    Re-added basis function classes

---
+++ src/BasisFunctions/Triangle_P1.cpp
@@ -0,0 +1,54 @@
+/*
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+#include "Triangle_P1.hpp"
+
+#include "Core/AnalysisModel.hpp"
+#include "Core/DomainManager.hpp"
+
+using namespace broomstyx;
+
+Triangle_P1::Triangle_P1() {}
+
+Triangle_P1::~Triangle_P1() {}
+
+RealVector Triangle_P1::giveBasisFunctionsAt( const RealVector& coor )
+{
+    double xi = coor(0);
+    double eta = coor(1);
+    
+    RealVector psi({1. - xi - eta,
+                    xi,
+                    eta});
+                    
+    return psi;
+}
+
+std::vector<RealVector> Triangle_P1::giveBasisFunctionDerivativesAt( const RealVector& coor )
+{
+    std::vector<RealVector> dpsi;
+    dpsi.assign(2, RealVector());
+    
+    dpsi[ 0 ] = {-1., 1., 0.};
+    dpsi[ 1 ] = {-1., 0., 1.};
+    
+    return dpsi;
+}
